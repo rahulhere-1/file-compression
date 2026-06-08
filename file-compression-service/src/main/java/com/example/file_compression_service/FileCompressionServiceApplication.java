1cb4eea075b47be01ce2ebf99e0846ff37f98d18
rahulhere-1 <rmendigeri4@gmail.com>
2026-06-08 22:52:45 +0530

    initial commit

---
+++ file-compression-service/src/main/java/com/example/file_compression_service/FileCompressionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.file_compression_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCompressionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCompressionServiceApplication.class, args);
+	}
+
+}
---
+++ file-compression-service/src/test/java/com/example/file_compression_service/FileCompressionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_compression_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCompressionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
