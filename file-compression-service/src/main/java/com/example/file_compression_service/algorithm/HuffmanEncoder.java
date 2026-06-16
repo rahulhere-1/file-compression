ee7e197d8d4109b19a155091d71a7fa67afd4b73
rahulhere-1 <rmendigeri4@gmail.com>
2026-06-16 20:38:14 +0530

    implementing huffman encoder algorithm - calculating character frequencies

--- file-compression-service/src/main/java/com/example/file_compression_service/algorithm/HuffmanEncoder.java
+++ file-compression-service/src/main/java/com/example/file_compression_service/algorithm/HuffmanEncoder.java
@@ -1,4 +1,28 @@
 package com.example.file_compression_service.algorithm;
 
+import com.example.file_compression_service.base.CodeTree;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class HuffmanEncoder {
+
+    private final CodeTree codeTree;
+
+    HuffmanEncoder(CodeTree codeTree){
+        this.codeTree = codeTree;
+    }
+
+    public void compress(String text){
+        codeTree.setCharacterFrequencyMap(calculateCharacterFrequencies(text));
+
+    }
+
+    private Map<Character,Integer> calculateCharacterFrequencies(String text){
+        Map<Character,Integer> characterFrequencyMap = new HashMap<>();
+        for(char ch: text.toCharArray()){
+            characterFrequencyMap.put(ch,characterFrequencyMap.getOrDefault(ch, 0) + 1);
+        }
+        return characterFrequencyMap;
+    }
 }
--- file-compression-service/src/main/java/com/example/file_compression_service/base/CodeTree.java
+++ file-compression-service/src/main/java/com/example/file_compression_service/base/CodeTree.java
@@ -1,12 +1,20 @@
 package com.example.file_compression_service.base;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CodeTree {
 
     private Node head;
+    private String text;
+    private Map<Character,String> huffmanCodes;
+    private Map<Character,Integer> characterFrequencyMap;
 
 
     public CodeTree(Node head) {
         this.head = head;
+        huffmanCodes = new HashMap<>();
+        characterFrequencyMap = new HashMap<>();
     }
 
 
@@ -16,5 +24,21 @@ public class CodeTree {
 
     public void setHead(Node head) {
         this.head = head;
+    }
+
+    public Map<Character, String> getHuffmanCodes() {
+        return huffmanCodes;
+    }
+
+    public void setHuffmanCodes(Map<Character, String> huffmanCodes) {
+        this.huffmanCodes = huffmanCodes;
+    }
+
+    public Map<Character, Integer> getCharacterFrequencyMap() {
+        return characterFrequencyMap;
+    }
+
+    public void setCharacterFrequencyMap(Map<Character, Integer> characterFrequencyMap) {
+        this.characterFrequencyMap = characterFrequencyMap;
     }
 }
